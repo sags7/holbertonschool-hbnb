d93c62656c3f4e93f5e8afeb6f6110158c239557
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-13 15:42:14 -0500

    place details now shows amenities

--- part4/base_files/scripts/place.js
+++ part4/base_files/scripts/place.js
@@ -56,8 +56,13 @@ document.addEventListener('DOMContentLoaded', () => {
         .innerHTML = `<div><p><strong>Host:</strong> ${data.owner.first_name} ${data.owner.last_name}</p></div>`;
       document.getElementById('place-price')
         .innerHTML = `<div><p><strong>Price:</strong> $${data.price}</p></div>`;
+      document.getElementById('place-amenities').innerHTML = `
+        <div>
+          <p><strong>Amenities:</strong> ${data.amenities.map(amenity => amenity.name).join(', ')}</p>
+        </div>
+      `;
       document.getElementById('place-details')
-        .innerHTML = `<div><p>${data.description}</p></div>`;
+        .innerHTML = `<div> <p>${data.description}</p></div > `;
     })
     .then(() => {
       fetch(`http://127.0.0.1:5000/api/v1/places/${placeId}/reviews`)
